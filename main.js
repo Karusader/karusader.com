4097c1d671635754404cd8d172f43a9d95dcd6d7
Karu <39348820+Karusader@users.noreply.github.com>
2023-07-07 23:30:30 -0700

    wow

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+if (Math.floor(Math.random() * 99) == 55){
+	document.body.style.backgroundImage = "url('images/200w.gif')"; 
+} else {
+	//document.body.style.backgroundImage = "url('images/200w.gif')"; 
+}
